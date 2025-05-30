76e9bc45f5c9f9abdb070e00a5c9c46a5c8c9377
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-30 14:17:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
