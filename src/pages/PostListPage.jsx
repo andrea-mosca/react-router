673cb66167802f7bb8647bdca2bdae5891449dc9
andrea-mosca <andrea.moscateli@gmail.com>
2025-06-03 16:08:52 +0200

    api index posts

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,52 +1,28 @@
-const posts = [
-  {
-    title: "post 1",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-  {
-    title: "post 2",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-  {
-    title: "post 3",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-  {
-    title: "post 4",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-  {
-    title: "post 5",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-  {
-    title: "post 6",
-    src: "https://www.shutterstock.com/shutterstock/photos/1037719192/display_1500/stock-vector-default-ui-image-placeholder-for-wireframes-for-apps-and-websites-1037719192.jpg",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo fugiat debitis itaque saepe mollitia nulla nostrum voluptate at illo. Iure a tempore laboriosam, ut assumenda saepe alias, quisquam odit ratione repellat pariatur rerum eveniet quam",
-  },
-];
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function PostListPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res.data.data);
+      setPosts(res.data.data);
+    });
+  }, []);
   return (
     <div className="container mt-4">
       <div className="row row-cols-3">
-        {posts.map((post, index) => (
-          <div className="col g-3" key={index}>
+        {posts.map((post) => (
+          <div className="col g-3" key={post.id}>
             <div className="card">
-              <img src={post.src} alt={post.name} />
+              <div className="card-header fw-bolder fs-4">{post.title}</div>
+              <img
+                src={`http://localhost:3000/` + post.image}
+                alt={post.title}
+              />
               <div className="card-body">
-                <p className="card-text">{post.description}</p>
+                <p className="card-text">{post.content}</p>
               </div>
             </div>
           </div>
